de730517469546b223a236f27d63c729aad2340f
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2024-03-12 22:37:07 +0100

    Add console app

---
+++ WinFo/WinFo.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿using WinFo.Service;
+using WinFo.Service.Usage;
+
+IServiceFactory sf = ServiceFactoryProducer.GetServiceFactory();
+
+IInstalledProgramService ips = sf.CreateInstalledProgramService();
+
+var progras = ips.GetInstalledPrograms();
+
+Console.WriteLine(progras.Count);
